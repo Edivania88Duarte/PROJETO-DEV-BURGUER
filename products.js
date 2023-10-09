fa2962d03f2646c37a1626569bd7a95e9ab5836c
Edivania88Duarte <edivania88duarte@gmail.com>
2023-10-08 21:48:05 -0300

    PROJETO DEV BURGUER

---
+++ products.js
@@ -0,0 +1,18 @@
+const menuOptions = [
+    { name: 'X-Salada', price: 30, vegan: false, src: './Assets/xsalada.jpeg' },
+    { name: 'X-Bacon', price: 34, vegan: false, src: './Assets/xbacon.png' },
+    { name: 'X-Bacon Egg', price: 39, vegan: false, src: './Assets/bacon-egg.png' },
+    { name: 'X-Monstruoso', price: 50, vegan: false, src: './Assets/monstruoso.png' },
+    { name: 'Big Vegano', price: 55, vegan: true, src: './Assets/xvegan.png' },
+    { name: 'X-Vegan', price: 30, vegan: true, src: './Assets/monstruoso-vegan.png' },
+    { name: 'X-Light', price: 35, vegan: true, src: './Assets/X-Light.jpg' },
+    { name: 'X-Croissant', price: 45, vegan: true, src: './Assets/X-Croissant.jpg' },
+    { name: 'X-Integral', price: 37, vegan: true, src: './Assets/X-Integral.jpg' },
+    { name: 'X-Burger', price: 25, vegan: false, src: './Assets/X-Burguer.jpg' },
+    { name: 'X-Tasty', price: 65, vegan: false, src: './Assets/X-Tasty.jpg' },
+    { name: 'X-American', price: 60, vegan: false, src: './Assets/X-American.jpg'},
+
+
+
+]
+
---
+++ scriptResp.js
@@ -0,0 +1,11 @@
+document.querySelector('.menuMobile').addEventListener('click', function() {
+    document.querySelector('.navLinks').classList.toggle('active');
+  })
+
+
+  window.addEventListener("load", function () {
+    if (window.innerWidth < 768) {
+        backgroundVideo.removeAttribute("autoplay");
+        playButton.style.display = "block"; // Mostra o botão de reprodução
+    }
+});
